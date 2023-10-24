25741f52244639bd3e0af9b7fa2f13b149b3edf2
ศิลา เมฆศิขริน <silastudyandwork@gmail.com>
2023-10-24 11:35:04 +0700

    Comparing Numbers (a, b)

---
+++ Comparing Number/comparing.py
@@ -0,0 +1,11 @@
+#comparing if a > or < b
+
+a = float(input("value a : "))
+b = float(input("value b : "))
+
+if a>b :
+    print(f"{a} > {b}")
+if a<b :
+    print(f"{a} < {b}")
+else :
+    print(f"{a} == {b}")
