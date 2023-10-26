f9862913fc0505e3e5d10407cab4624ee01d3128
ศิลา เมฆศิขริน <silastudyandwork@gmail.com>
2023-10-26 22:13:10 +0700

    change break method

--- PYTHON/FIND MAX MIN/max-min.py
+++ PYTHON/FIND MAX MIN/max-min.py
@@ -1,15 +1,17 @@
-#this problem I define that can input as much as want. it will stop when type 000
+#this problem I define that can input as much as want. it will stop when type "STOP"
 
 max = 0
 min = 0
 
 while True :
-    number = float(input("input your number : "))
+    number = input("input your number or 'STOP' to stop: ")
+    if number == "STOP":
+        break
+    number = float(number)
     if number>max :
         max = number
     if number<min:
         min = number
-    if number == 000:
-        break
+
 print("max :", max)
 print("min :", min)
