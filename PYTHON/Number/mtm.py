571c42c851ceb496b4cc368d8851394cc904f67e
ศิลา เมฆศิขริน <silastudyandwork@gmail.com>
2023-10-31 00:48:49 +0700

    update mtm

--- PYTHON/Number/mtm.py
+++ PYTHON/Number/mtm.py
@@ -9,6 +9,6 @@ while True:
         break
 a=sorted(list)
 b=sorted(list, reverse=True)
-#list.sort() can use this instead.
+#list.sort(), list.reverse() can use this instead.
 print("Min to Max :", a)
 print("Max to Min :", b)
