e303fbfefe3025f4e15b0a6fe6ebb021aa6bf353
ศิลา เมฆศิขริน <silastudyandwork@gmail.com>
2023-10-31 15:05:46 +0700

    add odd even

--- PYTHON/Number/mtm.py
+++ PYTHON/Number/mtm.py
@@ -1,10 +1,16 @@
 #min to max and max to min
 list = []
+even = []
+odd = []
 
 while True:
     number = int(input("input int: "))
     if number >= 0:
         list.append(number)
+        if number%2==0:
+            even.append(number)
+        else:
+            odd.append(number)
     else:
         break
 a=sorted(list)
@@ -12,3 +18,5 @@ b=sorted(list, reverse=True)
 #list.sort(), list.reverse() can use this instead.
 print("Min to Max :", a)
 print("Max to Min :", b)
+print("Odd :", odd)
+print("Even :", even)
